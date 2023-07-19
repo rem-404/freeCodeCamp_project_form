9054ad37b653925e875581eb91fd3685dd53283b
rem-404 <rednation37@gmail.com>
2023-07-19 13:41:28 +0800

    webpack commit (feature_handling_css)
    configure webpack for development and  production

---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: "asset/resource"
+      },
+    ],
+  },
+};
